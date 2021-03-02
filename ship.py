4fff197d9fcde5b174faf122e1219e1070de644c
Wesley Tandy <Wesley_Tandy1@baylor.edu>
2021-03-02 16:44:51 -0600

    gave ship boundry within screen

--- ship.py
+++ ship.py
@@ -7,6 +7,7 @@ class Ship:
     def __init__(self, ai_game):
         """Initializa the ship and set its starting position"""
         self.screen = ai_game.screen
+        self.settings = ai_game.settings
         self.screen_rect = ai_game.screen.get_rect()
 
         # load the ship image and get its rect.
@@ -18,12 +19,19 @@ class Ship:
         self.moving_right = False
         self.moving_left = False
 
+        # store a decimal value for the ships horizontal position
+        self.x = float(self.rect.x)
+
     def update(self):
         """update the ships position based on the movement falg"""
-        if self.moving_right:
-            self.rect.x += 1
-        if self.moving_left:
-            self.rect.x -= 1
+        # Update the ships x value, not the rect.
+        if self.moving_right and self.rect.right < self.screen_rect.right:
+            self.x += self.settings.ship_speed
+        if self.moving_left and self.rect.left > 0:
+            self.x -= self.settings.ship_speed
+
+        # update rect from self.x
+        self.rect.x = self.x
 
     def blitme(self):
         """Draw the ship at its current location"""
