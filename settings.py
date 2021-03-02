1d4b25f4b82579214c67812e82fbac8612d26913
Wesley Tandy <Wesley_Tandy1@baylor.edu>
2021-03-02 15:57:18 -0600

    Created initial setup and settings no hard code

---
+++ settings.py
@@ -0,0 +1,9 @@
+class Settings:
+    """ A class to store all the settings for alien invasion"""
+
+    def __init__(self):
+        """Initialize games settings"""
+        # screen settings
+        self.screen_width = 1200
+        self.screen_height = 800
+        self.bg_color = (230, 230, 230)
