6e2af050ecb0a9649666f9f97c549aaa86624b13
Wesley Tandy <Wesley_Tandy1@baylor.edu>
2021-03-07 22:42:23 -0600

    alien to appear in top right

--- alien_invasion.py
+++ alien_invasion.py
@@ -5,6 +5,7 @@ import pygame
 from settings import Settings
 from ship import Ship
 from bullet import Bullet
+from alien import Alien
 
 
 class AlienInvasion:
@@ -23,6 +24,9 @@ class AlienInvasion:
 
         self.ship = Ship(self)
         self.bullets = pygame.sprite.Group()
+        self.aliens = pygame.sprite.Group()
+
+        self._create_fleet()
 
         # set the background color
         # self.bg_color = (230.230, 230)
@@ -88,9 +92,16 @@ class AlienInvasion:
         self.ship.blitme()
         for bullet in self.bullets.sprites():
             bullet.draw_bullet()
+        self.aliens.draw(self.screen)
 
         # Make the most recently drawn screen visible
         pygame.display.flip()
+
+    def _create_fleet(self):
+        """Create the fleet of aliens"""
+        # make an alien
+        alien = Alien(self)
+        self.aliens.add(alien)
 
 
 if __name__ == "__main__":
