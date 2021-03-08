5c7b89bf11151f22a1ecd319d34d61dba2392dd8
Wesley Tandy <Wesley_Tandy1@baylor.edu>
2021-03-07 22:28:33 -0600

    created update bullet method

--- alien_invasion.py
+++ alien_invasion.py
@@ -33,13 +33,7 @@ class AlienInvasion:
             self._check_events()
             self.ship.update()
             self.bullets.update()
-
-            # Get rid of bullets that have disapeared
-            for bullet in self.bullets.copy():
-                if bullet.rect.bottom <= 0:
-                    self.bullets.remove(bullet)
-            print(len(self.bullets))
-
+            self._update_bullets()
             self._update_screen()
 
     def _check_events(self):
@@ -76,6 +70,17 @@ class AlienInvasion:
         if len(self.bullets) < self.settings.bullets_allowed:
             new_bullet = Bullet(self)
             self.bullets.add(new_bullet)
+
+    def _update_bullets(self):
+        """Update position of bullets and get rid of old bullets"""
+        # Update bullet position
+        self.bullets.update()
+
+        # Get rid of bullets that have disapeared
+        for bullet in self.bullets.copy():
+            if bullet.rect.bottom <= 0:
+                self.bullets.remove(bullet)
+        print(len(self.bullets))
 
     def _update_screen(self):
         """ Update images on the screen, and flip to the new screen"""
